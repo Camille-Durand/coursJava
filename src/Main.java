1b2fdaa6f7dc2a44755ab85082d741c28ed4a241
Camille-Durand <camillefanguin@gmail.com>
2024-03-20 14:19:47 +0100

    Début création game (Personnage) + déplacement exos précédent

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,5 @@
-import com.cda.classe.Habitation;
-import com.cda.classe.Vehicule;
-
 public class Main {
     public static void main(String[] args) {
-        /*
-        Habitation home = new Habitation("Home",3,5,1);
-        Habitation casa = new Habitation("Casa",4,6,2);
-        Habitation haus = new Habitation("Haus",5,7,3);
-
-        System.out.println("L’habitation : " + home.getNom() + " à une surface de: " + home.surface() + "m²");
-        System.out.println("L’habitation : " + casa.getNom() + " à une surface de: " + casa.surface() + "m²");
-        System.out.println("L’habitation : " + haus.getNom() + " à une surface de: " + haus.surface() + "m²");
-         */
-
-        Vehicule milletipla = new Vehicule("1000tipla",4,287);
-        Vehicule motobylette = new Vehicule("Motobylette",2,150);
-        Vehicule concept = new Vehicule("Concept",3,75);
-
-        System.out.println("Le vehicule " + milletipla.getNom() + " est de type " + milletipla.detect());
-        System.out.println("Le vehicule " + motobylette.getNom() + " est de type " + motobylette.detect());
-        System.out.println("Le vehicule " + concept.getNom() + " est de type " + concept.detect());
-
-        milletipla.boost();
-        motobylette.boost();
-        concept.boost();
-
-        System.out.println("Le vehicule " + milletipla.getNom() + " possède une vitesse boostée de " + milletipla.getVitesse() + "km/h");
-        System.out.println("Le vehicule " + motobylette.getNom() + " possède une vitesse boostée de " + motobylette.getVitesse() + "km/h");
-        System.out.println("Le vehicule " + concept.getNom() + " possède une vitesse boostée de " + concept.getVitesse() + "km/h");
-
-        System.out.println("Le véhicule le + rapide est: " + motobylette.plusRapide(concept));
-    }
+            
+        }
 }
---
+++ src/com/cda/game/Personnage.java
@@ -0,0 +1,48 @@
+package com.cda.game;
+
+public class Personnage {
+    private String nom;
+    private int vie;
+    private int attaque;
+    private int defense;
+
+    public Personnage(){}
+    public Personnage(String nom, int vie, int attaque, int defense){
+        this.nom = nom;
+        this.vie = vie;
+        this.attaque = attaque;
+        this.defense = defense;
+    }
+
+    public String getNom(){
+        return nom;
+    }
+    public void setNom(){
+        this.nom = nom;
+    }
+
+    public int getVie(){
+        return vie;
+    }
+    public void setVie(){
+        this.vie = vie;
+    }
+
+    public int getAttaque(){
+        return attaque;
+    }
+    public void setAttaque(){
+        this.attaque = attaque;
+    }
+
+    public int getDefense(){
+        return defense;
+    }
+    public void setDefense(){
+        this.defense = defense;
+    }
+
+    public void attaquer(Personnage cible){
+        cible.vie -= this.attaque - cible.defense;
+    }
+}
